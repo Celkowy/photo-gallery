6f63568f0491acaece87de38dff2b6377a1b77d9
tucelek@gmail.com <tucelek@gmail.com>
2020-06-21 22:52:48 +0200

    major fixes

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ lightbox.option({
 const builder = document.querySelector('.nav-ul li:nth-of-type(2)');
 const whyThisGallery = document.querySelector('.nav-ul li:nth-of-type(3)');
 const services = document.querySelector('.nav-ul li:last-of-type');
-
 const topMenu = document.querySelector('.top-menu');
 const switchColor = document.querySelectorAll('.switch-color');
 const nav = document.querySelector('nav');
@@ -23,18 +22,15 @@ const rotate1 = document.querySelector('.rotate1');
 const rotate2 = document.querySelector('.rotate2');
 const rotate3 = document.querySelector('.rotate3');
 
+let navSwitcher = [navLi1, navLi2, navLi3];
 
-navLi1.addEventListener('click', function(){
-  rotate1.classList.toggle('rotate')
-})
-
-navLi2.addEventListener('click', function(){
-  rotate2.classList.toggle('rotate')
-})
-
-navLi3.addEventListener('click', function(){
-  rotate3.classList.toggle('rotate')
-})
+for(let i = 0; i < navSwitcher.length; i++){
+  navSwitcher[i].addEventListener('click', function(){
+    rotate1.classList.toggle('rotate')
+    rotate2.classList.toggle('rotate')
+    rotate3.classList.toggle('rotate')
+  })
+}
 
 hamburger.addEventListener('click', function(){
   topSlideMenu.classList.toggle('active');
@@ -49,7 +45,6 @@ exitI.addEventListener('click', function(){
   exitI.classList.remove('show');
 })
 
-
 let topMenuTriggers = [builder, whyThisGallery, services];
 
 for(let i = 0; i < topMenuTriggers.length; i++){
@@ -57,11 +52,8 @@ for(let i = 0; i < topMenuTriggers.length; i++){
   topMenu.classList.toggle('active')
   nav.classList.toggle('black');
 
-
   switchColor.forEach(function(color){
     color.classList.toggle('black')
       })
 })
 }
-
-
