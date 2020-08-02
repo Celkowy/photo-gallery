abafab3027de772dd330202fe0d534b14070612a
tucelek@gmail.com <tucelek@gmail.com>
2020-08-03 00:43:08 +0200

    pushState add

--- script.js
+++ script.js
@@ -35,6 +35,13 @@ hamburger.addEventListener('click', function(){
   topSlideMenu.classList.toggle('active');
   document.body.classList.add('do-not-move')
   exitI.classList.add('show');
+  history.pushState({page: 0}, "")
+})
+
+window.addEventListener('popstate', () => {
+  topSlideMenu.classList.remove('active')
+  document.body.classList.remove('do-not-move')
+  exitI.classList.remove('show');
 })
 
 exitI.addEventListener('click', function(){
